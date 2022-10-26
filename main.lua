edd06748adcbee6a193fe83ba1987055e01bd2ac
Jay <96400085+realotfjay@users.noreply.github.com>
2022-10-26 15:38:07 -0400

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,3 +1,18 @@
 ESX = nil
+local PlayerData = {}
 
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+Citizen.CreateThread(function()
+    while ESX == nil do
+        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+        Citizen.Wait(0)
+end)
+
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function(xPlayer)
+    PlayerData = xPlayer
+end)
+
+RegisterNetEvent('esx:setJob')
+AddEventHandler('esx:setJob', function(job)
+    PlayerData.job = job
+end)
